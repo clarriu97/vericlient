0710a483ac26c1bc4d9b52a14ef2a9ee87fa006e
Carlos Larriu <larriucarlos@gmail.com>
2024-06-30 19:10:12 +0200

    added config to the project

---
+++ src/vericlient/config/config.py
@@ -0,0 +1,17 @@
+"""
+Configuration for the application.
+"""
+import os
+
+from dynaconf import Dynaconf
+
+
+current_directory = os.path.dirname(os.path.realpath(__file__))
+basepath = os.path.dirname(__file__)
+config_path = os.path.join(basepath, "config.yml")
+settings = Dynaconf(
+    root_path=current_directory,
+    settings_files=[config_path],
+    envvar_prefix="VCSP",
+    # environments=True,
+)
