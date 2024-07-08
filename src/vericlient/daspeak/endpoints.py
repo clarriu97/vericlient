8e0adfcb4ec4ca1f124ca5ef3cd46747c55cb733
Carlos Larriu <larriucarlos@gmail.com>
2024-07-08 18:37:44 +0200

    added daspeak endpoints and input/output models

--- src/vericlient/daspeak/endpoints.py
+++ src/vericlient/daspeak/endpoints.py
@@ -5,4 +5,14 @@ from vericlient.endpoints import Endpoints
 
 
 class DaspeakEndpoints(Enum):
-    alive = Endpoints.ALIVE.value
+    ALIVE = Endpoints.ALIVE.value
+    MODELS = "models"
+    MODELS_METADATA = "models/metadata"
+    MODELS_CALIBRATION = "models/calibration"
+    MODELS_METADATA_FROM_CREDENTIAL = "models/metadata/from-credential"
+    MODELS_HASH_CREDENTIAL_WAV = "models/<hash>/credential/wav"
+    SIMILARITY_CREDENTIAL2CREDENTIAL = "similarity/credential2credential"
+    SIMILARITY_CREDENTIAL2WAV = "similarity/credential2wav"
+    SIMILARITY_WAV2WAV = "similarity/wav2wav"
+    IDENTIFICATION_WAV2CREDENTIALS = "identification/wav2credentials"
+    IDENTIFICATION_CREDENTIAL2CREDENTIALS = "identification/credential2credentials"
---
+++ src/vericlient/daspeak/models.py
@@ -0,0 +1,123 @@
+"""Module to define the models for the Daspeak API"""
+from typing import Union
+from io import BytesIO
+
+from pydantic import BaseModel
+
+
+class DaspeakResponse(BaseModel):
+    version: str
+    status_code: int
+
+
+class ModelsOutput(DaspeakResponse):
+    models: list
+
+
+class ModelsMetadataInput(BaseModel):
+    hash: str
+
+
+class ModelsMetadata(BaseModel):
+    hash: str
+    description: str
+
+
+class ModelsMetadataOutput(DaspeakResponse):
+    metadata: ModelsMetadata
+
+
+class ModelsCalibrationInput(BaseModel):
+    hash: str
+
+
+class ModelsCalibrationOutput(DaspeakResponse):
+    hash: str
+    calibrations: list
+
+
+class ModelsMetadataFromCredentialInput(BaseModel):
+    credential: str
+
+
+class ModelsMetadataFromCredentialOutput(DaspeakResponse):
+    metadata: ModelsMetadata
+
+
+class ModelsHashCredentialWavInput(BaseModel):
+    audio: Union[str, BytesIO]
+    hash: str
+    channel: int = 1
+    calibration: str = "telephone-channel"
+
+
+class ModelMetadata(BaseModel):
+    hash: str
+    mode: str
+
+
+class ModelsHashCredentialWavOutput(DaspeakResponse):
+    model: ModelMetadata
+    credential: str
+    authenticity: float
+    input_audio_duration: float
+    net_speech_duration: float
+
+
+class SimilarityCredential2CredentialInput(BaseModel):
+    credential_reference: str
+    credential_to_evaluate: str
+    caliration: str = "telephone-channel"
+
+
+class SimilarityCredential2CredentialOutput(DaspeakResponse):
+    calibration: str
+    score: float
+
+
+class SimilarityCredential2WavInput(BaseModel):
+    credential_reference: str
+    audio_to_evaluate: Union[str, BytesIO]
+    channel: int = 1
+    calibration: str = "telephone-channel"
+
+
+class SimilarityCredential2WavOutput(DaspeakResponse):
+    score: float
+    model: ModelMetadata
+    calibration: str
+    authenticity_to_evaluate: float
+    input_audio_duration: float
+    net_speech_duration: float
+
+
+class SimilarityWav2WavInput(BaseModel):
+    audio_reference: Union[str, BytesIO]
+    audio_to_evaluate: Union[str, BytesIO]
+    channel_reference: int = 1
+    channel_to_evaluate: int = 1
+    calibration: str = "telephone-channel"
+
+
+class SimilarityWav2WavOutput(DaspeakResponse):
+    score: float
+    model: ModelMetadata
+    calibration: str
+    authenticity_reference: float
+    authenticity_to_evaluate: float
+    input_audio_duration_reference: float
+    input_audio_duration_to_evaluate: float
+    net_speech_duration_reference: float
+    net_speech_duration_to_evaluate: float
+
+
+class IdentificationCredential2CredentialInput(BaseModel):
+    credential_reference: str
+    credentials_list: list
+    calibration: str = "telephone-channel"
+
+
+class IdentificationCredential2CredentialOutput(DaspeakResponse):
+    calibration: str
+    model: ModelMetadata
+    scores: list
