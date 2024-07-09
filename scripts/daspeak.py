8ccdaef5d527a84192f18f6113bdad4b0c0b0cbc
Carlos Larriu <clarriu@veridas.com>
2024-07-09 17:14:34 +0200

    updated daspeak example script

--- scripts/daspeak.py
+++ scripts/daspeak.py
@@ -3,6 +3,7 @@ Example script to demonstrate how to use the daspeak module.
 """
 from vericlient import VericlientFactory, APIs
 
-client = VericlientFactory.get_client(api=APIs.DASPEAK, apikey="your_api_key")
+client = VericlientFactory.get_client(APIs.DASPEAK.value, apikey="your_api_key")
 
 print(client.alive())
+print(client.get_models())
