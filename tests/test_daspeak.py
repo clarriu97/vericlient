f99c3a9d0d1e23bae65979ea5f08954792f95119
Carlos Larriu <larriucarlos@gmail.com>
2024-08-16 18:46:22 +0200

    daspeak client: added test to get api models

--- tests/test_daspeak.py
+++ tests/test_daspeak.py
@@ -28,3 +28,28 @@ def test_daspeak_alive(mock_server, endpoint, mock_response, mock_status_code, u
     response = daspeak_client.alive()
 
     assert response
+
+
+@pytest.mark.parametrize(
+    "endpoint, mock_response, mock_status_code, url, target, environment, location",
+    [
+        ('https://api-work.eu.veri-das.com/daspeak/v1/models', {"version": "", "models": ["model1", "model2"]}, 200, None, Target.CLOUD.value, Environments.SANDBOX.value, Locations.EU.value),
+    ]
+)
+def test_daspeak_get_models(mock_server, endpoint, mock_response, mock_status_code, url, target, environment, location):
+    if mock_server:
+        mock_server.get(endpoint, json=mock_response, status_code=mock_status_code)
+
+    daspeak_client = DaspeakClient(
+        apikey="fake-apikey",
+        target=target,
+        environment=environment,
+        location=location,
+        url=url,
+    )
+    response = daspeak_client.get_models()
+
+    if mock_server:
+        assert response.models == mock_response["models"]
+    else:
+        assert isinstance(response.models, list)
