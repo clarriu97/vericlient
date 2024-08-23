8045b334d99531e6c8a462a3c2aa4a4e108c25b7
Carlos Larriu <larriucarlos@gmail.com>
2024-08-23 12:10:16 +0200

    Added two new common errors: UnsupportedMediaTypeError and InvalidCredentialError

--- src/vericlient/exceptions.py
+++ src/vericlient/exceptions.py
@@ -16,3 +16,19 @@ class AuthorizationError(Exception):
     def __init__(self) -> None:
         message = "The request was not authorized. Did you provide the correct API key?"
         super().__init__(message)
+
+
+class UnsupportedMediaTypeError(Exception):
+    """Exception raised for unsupported media type errors."""
+
+    def __init__(self) -> None:
+        message = "The media type of the request is not supported."
+        super().__init__(message)
+
+
+class InvalidCredentialError(Exception):
+    """Exception raised for invalid credentials."""
+
+    def __init__(self) -> None:
+        message = "The credential/s provided are invalid."
+        super().__init__(message)
